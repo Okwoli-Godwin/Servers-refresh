ae3b4fd4eba294820b645d42f2914c2dab7cc3bd
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-22 13:59:02 +0100

    done

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import express, { Application } from "express"
 import cors from "cors"
 import morgan from "morgan"
 import router from "./routes/userroutes"
+import imagerouter from "./routes/imagerouter"
 
 export const appConfig = (app: Application) => {
     app.use(express.json())
         .use(cors())
         .use(morgan("dev"))
 
-        app.use("/app", router)
+    app.use("/app", router)
+    app.use("/app/image", imagerouter)
 }
 
