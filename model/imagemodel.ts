92abc53af10753915220313ae17124f7a0accb0e
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-22 11:51:54 +0100

    update

---
+++ model/imagemodel.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+
+interface imageData{
+    course: string,
+    image: string,
+    name: string,
+    section: string,
+    summary: string
+}
+
+interface images extends imageData, mongoose.Document{ }
+
+const imageupload = new mongoose.Schema<imageData>(
+    {
+        course: {
+            type: String,
+            required: [true, "course is required"]
+        },
+        image: {
+            type: String,
+            required: [true, "image is required"]
+        },
+        name: {
+            type: String,
+            required: [true, "name is required"]
+        },
+        section: {
+            type: String,
+            required: [true, "section is required"]
+        },
+        summary: {
+            type: String,
+            required: [true, "summary is required"]
+        }
+    }
+)
+
+const imagemodel = mongoose.model("myImages", imageupload)
+
+export default imagemodel
