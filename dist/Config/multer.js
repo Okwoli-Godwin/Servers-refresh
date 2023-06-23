8947319d447b9e238af54d5cc1c34e2c799926bf
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-23 14:00:53 +0100

    saved for deployment

--- dist/Config/multer.js
+++ dist/Config/multer.js
@@ -5,13 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.coverUpload = void 0;
 const multer_1 = __importDefault(require("multer"));
-const storage = multer_1.default.diskStorage({
+const path_1 = __importDefault(require("path"));
+const Storage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads");
+        cb(null, path_1.default.join(__dirname, "../../Uploads"));
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniqueSuffix + path_1.default.extname(file.originalname));
+    },
 });
-const coverUpload = (0, multer_1.default)({ storage: storage }).single("coverImage");
+const coverUpload = (0, multer_1.default)({ storage: Storage }).single("coverImage");
 exports.coverUpload = coverUpload;
--- src/Config/multer.ts
+++ src/Config/multer.ts
@@ -1,28 +1,28 @@
 import multer from "multer"
-import {Request} from "express"
+import { Request } from "express"
+import path from "path"
 
 type DestinationCallback = (error: Error | null, destination: string) => void;
 type FilenameCallback = (error: Error | null, filename: string) => void;
 
-const storage = multer.diskStorage({
-    destination: (
-        req: Request,
-        file: Express.Multer.File,
-        cb: DestinationCallback
-    ) => {
-        cb(null, "uploads")
-    },
+const Storage = multer.diskStorage({
+  destination: (
+    req: Request,
+    file: Express.Multer.File,
+    cb: DestinationCallback
+  ) => {
+    cb(null, path.join(__dirname, "../../Uploads"));
+  },
 
-    filename: (
-        req: Request,
-        file: Express.Multer.File,
-        cb: FilenameCallback
-    ) => {
-        cb(null, file.originalname)
-    }
-})
+  filename: (req: Request, file: Express.Multer.File, cb: FilenameCallback) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
 
-
-const coverUpload = multer({ storage: storage }).single("coverImage");
+const coverUpload = multer({ storage: Storage }).single("coverImage");
 
 export { coverUpload };
