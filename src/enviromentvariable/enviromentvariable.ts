ac2d208194e06f07a0325e8c345e32d290e4789e
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-28 10:57:34 +0100

    update

--- src/enviromentvariable/enviromentvariable.ts
+++ src/enviromentvariable/enviromentvariable.ts
@@ -5,5 +5,6 @@ dotenv.config()
 export const enviromentvariables = {
     PORT: process.env.PORT as string,
     MONGODBCONNECT: process.env.MONGODBCONNECT as string,
-    Password: process.env.Password as string
+    Password: process.env.Password as string,
+    Email: process.env.Email as string
 }
