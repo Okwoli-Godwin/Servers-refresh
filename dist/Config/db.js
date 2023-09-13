e74d963e031cfc86573366ff1dfcae1f73f02925
Okwoli-Godwin <okwolig60@gmail.com>
2023-09-13 21:13:28 +0100

    update

--- dist/Config/db.js
+++ dist/Config/db.js
@@ -19,7 +19,7 @@ const URL = enviromentvariable_1.enviromentvariables.MONGODBCONNECT;
 const local = "mongodb://0.0.0.0:27017/uniabuja";
 const dbconnection = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const conn = yield mongoose_1.default.connect(local);
+        const conn = yield mongoose_1.default.connect(URL);
         if (mongoose_1.default.connection.host === "localhost") {
             console.log("connected to localhost");
         }
